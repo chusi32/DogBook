57bda8df1fe13a8513a8123ddf538571310f787f
chusi32 <chusims3224@gmail.com>
2016-11-05 08:57:23 +0100

    Ejemplo obtencion current user

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Auth;
+
 class HomeController extends Controller
 {
     /**
@@ -14,6 +16,7 @@ class HomeController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+
     }
 
     /**
@@ -23,6 +26,9 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+
+        $id = Auth::id();
+        $user = Auth::user()->name;
+        return view('home',['id' => $user]);
     }
 }
